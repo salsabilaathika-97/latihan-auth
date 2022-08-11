f3cb523161b01f598b743d8cf498e5f9b9e4f83b
salsabilaathika-97 <salsathikar@gmail.com>
2022-08-11 23:35:55 +0800

    add static API for register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,31 +1,52 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { useState } from "react";
+import axios from 'axios';
 
 const Register = () => {
-    return (
-        <>
-            <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [result, setResult] = useState("");
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-            </Form>
-        </>
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+
+        const payload = {
+            email : email,
+            password : password
+        }
+
+        axios
+        .post("https://reqres.in/api/register", payload)
+        .then((res) => setResult(res.data.token))
+        .catch((err) => console.log(err));
+    }
+
+    return (
+        <div style = {{ width:"100%" }}>
+            <div style = {{ width: "400px", margin: "0 auto" }}>
+                <h1>Register Page</h1>
+                <div style = {{ display: "flex", flexDirection: "column" }}>
+                    <label>Email</label>
+                    <input onChange={(e) => handleEmail(e)} type="text" />
+                </div>
+                <div style = {{ display: "flex", flexDirection: "column" }}>
+                    <label>Password</label>
+                    <input onChange={(e) => handlePassword(e)} type="password" />
+                </div>
+                <button onClick={handleRegister} style = {{ width: "100%", marginTop: "12px" }}>Register</button>
+                {
+                    !!result.length && <h1>Selamat, Anda Berhasil</h1>
+                }
+            </div>
+        </div>
     )
 }
 
