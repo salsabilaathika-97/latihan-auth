b7a2e543ae6ba0ef89822536e82c3147ff71f808
salsabilaathika-97 <salsathikar@gmail.com>
2022-08-11 22:05:00 +0800

    add register and login button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
         <>
             <h1>Home</h1>
+            <Link to={"/register"}>
+                <button>Register</button>
+            </Link>
+                <button>Login</button>
         </>
     )
 }
