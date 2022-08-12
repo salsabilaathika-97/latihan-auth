e756c7531f57e9816f487a552d77caad818896e6
salsabilaathika-97 <salsathikar@gmail.com>
2022-08-12 22:44:50 +0800

    testing add protected route in dashboard

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from './pages/Home';
 import Register from './pages/Register';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import ProtectedRoute from './HOC/ProtectedRoute';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [login, setLogin] = useState("");
+
+  useEffect(() => {
+      const token = localStorage.getItem('myToken')
+      if(token) {
+        setLogin(token);
+      }
+  }, []);
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />}/>
-        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/dashboard" element={
+          <ProtectedRoute isLogin={login}>
+              <Dashboard />
+          </ProtectedRoute>
+        }/>
       </Routes>
     </>
   );
