8207c89af9b689d03adb2528aa812de5e00e2d8a
salsabilaathika-97 <salsathikar@gmail.com>
2022-08-13 11:41:25 +0800

    get all user in dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 const Dashboard = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+       getData();
+      }, []);
+
+    const getData = () => {
+        axios
+        .get("https://reqres.in/api/users?page=2")
+        .then((res)=>setData(res.data.data))
+        .catch((err) => console.log(err));
+    }
+
     return (
-        <>
+        <div>
             <h1>Dashboard</h1>
-        </>
+            <div style = {{ display: "flex", flexWrap: "wrap" }}>
+            {
+                data.map((item)=> 
+                    <div style = {{ margin: "10px" }}>
+                        <p>{item.first_name}</p>
+                        <img src={item.avatar} />
+                        <div>
+                            <button>Edit</button>
+                            <button>Delete</button>
+                        </div>
+                    </div>
+                )
+            }
+            </div>
+        </div>
     )
 }
 
