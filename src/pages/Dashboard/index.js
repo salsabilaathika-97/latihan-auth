6665ca88b5725e4fba94ee75eaa5b5e99703448d
salsabilaathika-97 <salsathikar@gmail.com>
2022-08-13 12:38:10 +0800

    remove unused line

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
